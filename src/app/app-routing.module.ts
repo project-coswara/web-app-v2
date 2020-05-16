f1dcc099967bfc67eecb15efc74b915295eadaec
Anand Mohan <moghan.anand@gmail.com>
2020-05-16 23:31:11 +0530

    Added about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
 import {TeamComponent} from "./team/team.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
   {path: 'team', component: TeamComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {SharedModule} from "../shared.module";
 import {AppRoutingModule} from "./app-routing.module";
+import {AboutComponent} from "./about/about.component";
+import {AppComponent} from './app.component';
 import {HomeComponent} from "./home/home.component";
-import { TeamComponent } from './team/team.component';
+import {TeamComponent} from './team/team.component';
+import {SharedModule} from "../shared.module";
 
 @NgModule({
   declarations: [
+    AboutComponent,
     AppComponent,
     HomeComponent,
     TeamComponent
@@ -19,8 +21,7 @@ import { TeamComponent } from './team/team.component';
     SharedModule,
   ],
   providers: [],
-  exports: [
-  ],
+  exports: [],
   bootstrap: [AppComponent]
 })
 
